564eff4fad62ae9bd3b10246f7611728ed7f814c
Durgesh <nagarkardurgesh@gmail.com>
2021-06-17 13:13:11 +0530

    initial commit

---
+++ temp/stest1/sangrammarine/webform/js/script.js
@@ -0,0 +1,21 @@
+window.oncontextmenu = function () { return false }
+
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
+
+function validateForm() {
+    var x = document.forms["form1"]["email"].value;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        alert("Not a valid e-mail address");
+		document.write('Please Enter Valid E-Mail id');
+             return false;
+    }
+}
